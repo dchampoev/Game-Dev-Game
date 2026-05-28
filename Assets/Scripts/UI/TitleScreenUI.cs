25444ca6e8fa3919b3d0281de93395a1f2a40f34
dchampoev <danich04@abv.bg>
2026-05-28 12:12:27 +0300

    Select instructions button for keyboard flow

--- Assets/Scripts/UI/TitleScreenUI.cs
+++ Assets/Scripts/UI/TitleScreenUI.cs
@@ -6,14 +6,60 @@ using UnityEngine.UI;
 [ExcludeFromCodeCoverage]
 public class TitleScreenUI : MonoBehaviour
 {
+    const string InstructionsScreenName = "Instructions Screen";
+
     public Button firstButton;
+
+    GameObject instructionsScreen;
+    bool wasInstructionsOpen;
 
     void Start()
     {
-        if (firstButton != null)
+        instructionsScreen = GameObject.Find(InstructionsScreenName);
+        SelectButton(firstButton);
+    }
+
+    void Update()
+    {
+        if (!instructionsScreen)
+            instructionsScreen = GameObject.Find(InstructionsScreenName);
+
+        bool instructionsOpen = instructionsScreen && instructionsScreen.activeInHierarchy;
+
+        if (instructionsOpen)
         {
-            EventSystem.current.SetSelectedGameObject(null);
-            EventSystem.current.SetSelectedGameObject(firstButton.gameObject);
+            SelectInstructionsButtonIfNeeded();
         }
+        else if (wasInstructionsOpen || !HasCurrentSelection())
+        {
+            SelectButton(firstButton);
+        }
+
+        wasInstructionsOpen = instructionsOpen;
+    }
+
+    void SelectInstructionsButtonIfNeeded()
+    {
+        if (!instructionsScreen) return;
+
+        GameObject selectedObject = EventSystem.current ? EventSystem.current.currentSelectedGameObject : null;
+        if (selectedObject && selectedObject.activeInHierarchy && selectedObject.transform.IsChildOf(instructionsScreen.transform))
+            return;
+
+        SelectButton(instructionsScreen.GetComponentInChildren<Button>());
+    }
+
+    bool HasCurrentSelection()
+    {
+        GameObject selectedObject = EventSystem.current ? EventSystem.current.currentSelectedGameObject : null;
+        return selectedObject && selectedObject.activeInHierarchy;
+    }
+
+    void SelectButton(Button button)
+    {
+        if (!button || !EventSystem.current) return;
+
+        EventSystem.current.SetSelectedGameObject(null);
+        EventSystem.current.SetSelectedGameObject(button.gameObject);
     }
 }
